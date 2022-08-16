048ad53db7ed383ad0f37eb01944e9907e3b40d6
miyakiyu <sherry910509@gmail.com>
2022-08-16 14:39:27 +0800

    Add print board function
    Now can print by function

--- chess.kt
+++ chess.kt
@@ -1,25 +1,43 @@
+enum class Charater 
+{
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn,
+    Empty
+}
+
 fun main()
 {
-    val board: ArrayList<ArrayList<String>> = arrayListOf(
-        arrayListOf("-","-","-","-","-","-","-","-"),
-        arrayListOf("-","-","-","-","-","-","-","-"),
-        arrayListOf("-","-","-","-","-","-","-","-"),
-        arrayListOf("-","-","-","-","-","-","-","-"),
-        arrayListOf("-","-","-","-","-","-","-","-"),
-        arrayListOf("-","-","-","-","-","-","-","-"),
-        arrayListOf("-","-","-","-","-","-","-","-"),
-        arrayListOf("-","-","-","-","-","-","-","-"),
+    val board: ArrayList<ArrayList<Charater>> = arrayListOf(
+        arrayListOf(Charater.Rook,Charater.Knight,Charater.Bishop,Charater.Queen,Charater.King,Charater.Bishop,Charater.Knight,Charater.Rook),
+        arrayListOf(Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn),
+        arrayListOf(Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty),
+        arrayListOf(Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty),
+        arrayListOf(Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty),
+        arrayListOf(Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty,Charater.Empty),
+        arrayListOf(Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn,Charater.Pawn),
+        arrayListOf(Charater.Rook,Charater.Knight,Charater.Bishop,Charater.Queen,Charater.King,Charater.Bishop,Charater.Knight,Charater.Rook),
     )
 
-    for(i in board.indices)
+    println(print_board(board))
+    
+}
+
+
+fun print_board(board:ArrayList<ArrayList<Charater>>)
+{
+    for(i in 0..7)
     {
-        println(board[i].toString())
+        for(j in 0..7)
+        {
+            print(board[i][j])   
+            print(" ")
+        }
+        println("")
+        println("-----+-----+-----+-----+-----+-----+-----+-----")
     }
 }
 
-class chesses(var step:Int ,var status:String)
-enum class block()
-{
-    None,
-    Have
-}
